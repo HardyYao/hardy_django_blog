5d322e55606fc8b21ec49bf087503e958408bd23
Hardy Yao <hardyyao1995@gmail.com>
2017-07-23 11:48:11 +0800

    article show

--- hardy_blogs/urls.py
+++ hardy_blogs/urls.py
@@ -1,9 +1,12 @@
 """定义hardy_blogs的URL模式"""
 
 from django.conf.urls import url
-from .views import ArticleListView
+from . import views
+from .views import ArticleDetailView, ArticlePublishView, ArticleEditView
 
 urlpatterns = [
-    # 主页
-    url(r'^$', ArticleListView.as_view(), name='blog_index'),
+    url(r'^$', views.blog_index, name='blog_index'),
+    url(r'^article/publish$', ArticlePublishView.as_view(), name='article_publish'),
+    url(r'^article/(?P<title>\w+\.?\w+)$', ArticleDetailView.as_view(), name='article_detail'),
+    url(r'^article/(?P<title>\w+\.?\w+)/edit$', ArticleEditView.as_view(), name='article_edit'),
 ]
--- hardy_blogs/views.py
+++ hardy_blogs/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import F
-from django.views.generic.list import ListView
-
+from django.http import Http404
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic.detail import DetailView
 from .models import Article
+from django.views.generic.edit import FormView
+from hardy_blogs.forms import ArticlePublishForm
+from django.core.urlresolvers import reverse
 
 def blog_index(request):
     context = {
@@ -13,19 +14,58 @@ def blog_index(request):
     }
     return render(request, 'blog_index.html', context)
 
-class ArticleListView(ListView):
-    template_name = 'blog_index.html'
+class ArticlePublishView(FormView):
+    template_name = 'article_publish.html'
+    form_class = ArticlePublishForm
+    success_url = '/blog/'
 
-    def get_queryset(self, **kwargs):
-        object_list = Article.objects.all().order_by(F('created').desc())[:100]
-        paginator = Paginator(object_list, 10)
-        page = self.request.GET.get('page')
+    def form_valid(self, form):
+        form.save(self.request.user.username)
+        return super(ArticlePublishView, self).form_valid(form)
+
+class ArticleDetailView(DetailView):
+    template_name = 'article_detail.html'
+
+    def get_object(self, **kwargs):
+        title = self.kwargs.get('title')
         try:
-            object_list = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver first page.
-            object_list = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver last page of results.
-            object_list = paginator.page(paginator.num_pages)
-        return object_list
+            article = Article.objects.get(title=title)
+            article.views += 1
+            article.save()
+            article.tags = article.tags.split()
+        except Article.DoesNotExist:
+            raise Http404("Article does not exist")
+        return article
+
+class ArticleEditView(FormView):
+    template_name = 'article_publish.html'
+    form_class = ArticlePublishView
+    article = None
+
+    def get_initial(self, **kwargs):
+        title = self.kwargs.get('title')
+        try:
+            self.article = Article.objects.get(title=title)
+            initial = {
+                'title': title,
+                'content': self.article.content_md,
+                'tags': self.article.tags,
+            }
+            return initial
+        except Article.DoesNotExist:
+            raise Http404("Article does not exist")
+
+    def form_invalid(self, form):
+        form.save(self.request, self.article)
+        return super(ArticleEditView, self).form_valid(form)
+
+    def get_success_url(self):
+        title = self.request.POST.get('title')
+        success_url = reverse('article_detail', args=(title,))
+        return success_url
+
+class AdminRequiredMinin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(AdminRequiredMinin, cls).as_view(**initkwargs)
+        return staff_member_required(view)
